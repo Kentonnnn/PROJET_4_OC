0c4862a63277655909a8d1ee863e434376ecaa7b
Nelson PIRES DA SILVA <piresdasilva.n@gmail.com>
2023-03-02 12:36:38 +0100

    First files with fonts / images / MVC

---
+++ Projet_4_OC/Projet_4_OC/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Projet_4_OC
+//
+//  Created by Nelson Pires Da Silva on 3/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
